d8ea53cfbc6664cac2419879fb2702e282a84c45
Reinamora <reinamora@stampchain.io>
2025-04-03 21:23:29 -0500

    Update Bitcoin entrypoint script to properly configure ZMQ settings

--- docker/bitcoin-entrypoint.sh
+++ docker/bitcoin-entrypoint.sh
@@ -12,11 +12,23 @@ cat > /bitcoin/.bitcoin/bitcoin.conf << EOL
 # Created automatically by bitcoin-entrypoint.sh
 
 # Default RPC settings
-rpcuser=rpc
-rpcpassword=rpc
-rpcallowip=0.0.0.0/0
-rpcbind=0.0.0.0
-server=1
+rpcuser=${RPC_USER:-rpc}
+rpcpassword=${RPC_PASSWORD:-rpc}
+rpcallowip=${RPC_ALLOW_IP:-0.0.0.0/0}
+rpcbind=${RPC_BIND:-0.0.0.0}
+server=${SERVER:-1}
+listen=${LISTEN:-1}
+addresstype=${ADDRESS_TYPE:-legacy}
+txindex=${TX_INDEX:-1}
+prune=${PRUNE:-0}
+dbcache=${DB_CACHE:-4000}
+mempoolfullrbf=${MEMPOOL_FULL_RBF:-1}
+
+# ZMQ settings required by Counterparty
+zmqpubrawtx=${ZMQ_PUB_RAW_TX:-tcp://0.0.0.0:9332}
+zmqpubhashtx=${ZMQ_PUB_HASH_TX:-tcp://0.0.0.0:9332}
+zmqpubsequence=${ZMQ_PUB_SEQUENCE:-tcp://0.0.0.0:9332}
+zmqpubrawblock=${ZMQ_PUB_RAW_BLOCK:-tcp://0.0.0.0:9333}
 EOL
 
 # Create symlink to the config for bitcoin-cli in the root directory
@@ -26,7 +38,7 @@ ln -sf /bitcoin/.bitcoin/bitcoin.conf /root/.bitcoin/bitcoin.conf
 chmod 600 /bitcoin/.bitcoin/bitcoin.conf
 chmod 600 /root/.bitcoin/bitcoin.conf
 
-echo "Created bitcoin.conf with default RPC settings"
+echo "Created bitcoin.conf with RPC and ZMQ settings"
 echo "Starting bitcoind with command-line arguments..."
 
 # Execute bitcoind with all passed arguments
